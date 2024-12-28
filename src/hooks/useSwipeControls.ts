4b534f93ee7ad1ba76f3a4c9c931b399f3e9a3ea
tnm <t@ted.io>
2024-12-28 14:53:55 -0800

    control the mobile nav more

--- src/hooks/useSwipeControls.ts
+++ src/hooks/useSwipeControls.ts
@@ -15,15 +15,18 @@ export function useSwipeControls({
   const [touchEnd, setTouchEnd] = useState<number | null>(null);
 
   const onTouchStart: React.TouchEventHandler<HTMLDivElement> = (e) => {
+    e.preventDefault();
     setTouchEnd(null);
     setTouchStart(e.touches[0].clientX);
   };
 
   const onTouchMove: React.TouchEventHandler<HTMLDivElement> = (e) => {
+    e.preventDefault();
     setTouchEnd(e.touches[0].clientX);
   };
 
-  const onTouchEnd = () => {
+  const onTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
+    e.preventDefault();
     if (!touchStart || !touchEnd) return;
 
     const distance = touchStart - touchEnd;
