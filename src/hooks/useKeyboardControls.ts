abb998aef0826c6c0d460166f5d69172300ffeaf
tnm <t@ted.io>
2024-12-28 15:38:00 -0800

    use mapping

--- src/hooks/useKeyboardControls.ts
+++ src/hooks/useKeyboardControls.ts
@@ -47,16 +47,20 @@ export function useKeyboardControls({
         event.preventDefault();
       }
 
-      if (event.code === 'ArrowLeft' || event.code === 'KeyJ') {
-        onPrevious();
-      } else if (event.code === 'ArrowRight' || event.code === 'KeyK') {
-        onNext();
-      } else if (event.code === 'KeyF') {
-        onMarkKnown();
-      } else if (event.code === 'KeyD') {
-        onMarkUnknown();
-      } else if (event.code === 'KeyZ') {
-        onToggleFocus();
+      const keyActionMap: Record<string, (() => void) | undefined> = {
+        'ArrowLeft': onPrevious,
+        'KeyJ': onPrevious,
+        'ArrowRight': onNext,
+        'KeyK': onNext,
+        'KeyF': onMarkKnown,
+        'KeyD': onMarkUnknown,
+        'KeyZ': onToggleFocus,
+      };
+
+      // Special cases handled separately
+      const action = keyActionMap[event.code];
+      if (action) {
+        action();
       } else if (event.code === 'Escape' && focusMode) {
         onExitFocus();
       } else if (event.code.match(/^Digit[1-6]$/)) {
